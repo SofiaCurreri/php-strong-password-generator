7e860f7dbbeb8d16c7e4c6284db0ef2a582cfc66
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-13 17:14:07 +0100

    minor fix

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ echo $len_password = $_GET["password"];
 function randomPassword($len) {
     $elements = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     $elementsLen = strlen($elements) - 1;
-    $password = array[];
+    $password = [];
     foreach($i = 0; $i < $len; $i++) {
         $n = rand(0, $elementsLen);
         $password[] = $elements[$n];
